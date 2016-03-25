6469f22d57e952562d9f831566344e7dd5961702
Emiliano Santi <emisada@gmail.com>
2016-03-25 11:14:43 -0300

    [Added] Trigger event with query when Enter key is pressed - Code Review changes.

--- examples/async-data/app.js
+++ examples/async-data/app.js
@@ -7,8 +7,7 @@ let App = React.createClass({
   getInitialState () {
     return {
       unitedStates: getStates(),
-      loading: false,
-      valueEntered: ''
+      loading: false
     }
   },
 
@@ -22,52 +21,37 @@ let App = React.createClass({
           a server request with, then change state and pass in new items, it will
           attempt to autocomplete the first one.
         </p>
-        <Autocomplete {...this.getAutocompleteProps()} />
-        {this.renderMessage()}
+        <Autocomplete
+          labelText="Choose a state from the US"
+          inputProps={{name: "US state"}}
+          ref="autocomplete"
+          items={this.state.unitedStates}
+          getItemValue={(item) => item.name}
+          onSelect={(value, item) => {
+            // set the menu to only the selected item
+            this.setState({ unitedStates: [ item ] })
+            // or you could reset it to a default list again
+            // this.setState({ unitedStates: getStates() })
+          }}
+          onChange={(event, value) => {
+            this.setState({loading: true})
+            fakeRequest(value, (items) => {
+              this.setState({ unitedStates: items, loading: false })
+            })
+          }}
+          onSubmit={(value, setValue) => {
+            setValue(value.split(',').reverse().join())
+          }}
+          renderItem={(item, isHighlighted) => (
+            <div
+              style={isHighlighted ? styles.highlightedItem : styles.item}
+              key={item.abbr}
+              id={item.abbr}
+            >{item.name}</div>
+          )}
+        />
       </div>
     )
-  },
-
-  renderMessage () {
-    let dataToRender = null;
-
-    if (this.state.valueEntered) {
-      dataToRender = <p>Your search {this.state.valueEntered} is not in the list of states. Making service call...</p>;
-    }
-
-    return dataToRender;
-  },
-
-  getAutocompleteProps () {
-    return {
-      labelText: "Choose a state from the US",
-      inputProps: { name: "US state" },
-      ref: "autocomplete",
-      items: this.state.unitedStates,
-      getItemValue: (item) => item.name,
-      onSelect: (value, item) => {
-        // set the menu to only the selected item
-        this.setState({ unitedStates: [ item ] })
-        // or you could reset it to a default list again
-        // this.setState({ unitedStates: getStates() })
-      },
-      onChange: (event, value) => {
-        this.setState({loading: true})
-        fakeRequest(value, (items) => {
-          this.setState({unitedStates: items, loading: false})
-        })
-      },
-      onSearch: (value) => {
-        this.setState({valueEntered: value})
-      },
-      renderItem: (item, isHighlighted) => (
-        <div
-          style={isHighlighted ? styles.highlightedItem : styles.item}
-          key={item.abbr}
-          id={item.abbr}
-        >{item.name}</div>
-      )
-    };
   }
 })
 
